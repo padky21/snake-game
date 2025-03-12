415990cab7678e446aef1ccc6528a34852510e42
Vasco Teixeira <padky21@gmail.com>
2025-03-12 11:47:04 +0000

    Renders the board

--- src/render.c
+++ src/render.c
@@ -5,3 +5,12 @@ void menu(){
     printf("1 - New game\n");
     printf("2 - Exit\n");
 }
+
+void renderBoard(width, height){
+    for(int i = 0; i < height; i++){
+        for(int j = 0; j < width; j++){
+            printf(" 0 ");
+        }
+        printf("\n");
+    }
+}
